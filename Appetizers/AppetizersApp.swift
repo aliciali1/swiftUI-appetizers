e34b9f4424660154fe3c6af2be5c5924d21786c7
aliciali1 <159329207+aliciali1@users.noreply.github.com>
2024-06-19 16:31:08 -0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Alicia Li on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
